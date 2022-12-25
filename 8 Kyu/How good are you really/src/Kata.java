e489615a4d2beb04840ef7337cbc185c1a59a733
MikolajP13 <mikolajp1313@gmail.com>
2022-12-25 12:00:57 +0100

    update

--- 8 Kyu/How good are you really/src/Kata.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-public class Kata {
-    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
-        return Arrays.stream(classPoints).summaryStatistics().getAverage() < yourPoints;
-    }
-}
